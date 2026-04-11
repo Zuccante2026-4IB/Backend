b433a042d1f3372c0352e550afaf3e28eaafce14
samuele-brusegan <samuele.brusegan@itiszuccante.edu.it>
2026-04-11 09:49:19 +0200

    Trying to deploy (may fixed arch incomp.)

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi_db'),
+      port: env.int('DATABASE_PORT', 3307),
+      database: env('DATABASE_NAME', 'strapiDB'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
     },
