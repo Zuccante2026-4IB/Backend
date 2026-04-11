abb5037b2f4d4abebbfc664bc55d47939c38095e
samuele-brusegan <samuele.brusegan@gmail.com>
2026-04-11 10:40:18 +0200

    Probably fixed arch bug

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://strapi.brusegan.it'),
+//  url: env('PUBLIC_URL', 'https://strapi.brusegan.it'),
+  url: env('PUBLIC_URL', 'http://strapi.local'),
   app: {
     keys: env.array('APP_KEYS'),
   },
